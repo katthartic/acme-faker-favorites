05a3b10d5ea62390727f21fcfca89f63f59509ab
katthartic <katt.baum@gmail.com>
2020-02-06 21:21:31 -0500

    Complete workshop

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
-const users = Array(20).fill('').map(user => {
-    const isFavorite = !!Math.floor(Math.random() * 10)
+const addUser = () => {
+    const isFavorite = !!Math.round(Math.random(), 0)
     return {...faker.helpers.createCard(), isFavorite};
-})
+}
+
+const users = Array(20).fill('').map(user => addUser())
 
 console.log(users)
 
@@ -12,12 +13,38 @@ class App extends React.Component {
         this.state = {users}
     }
 
+
     render() {
+        const toggleUser = (user) => {
+            user.isFavorite = !user.isFavorite
+            console.log(user)
+            this.setState({users})
+        }
+
+        const favLength = users.filter(user => user.isFavorite).length
+
+        const titleLink = React.createElement('a', {
+                href: '#',
+                onClick: () => {
+                    const newUser = addUser()
+                    this.setState({users: users.unshift(newUser)})
+                }
+            }, `You have ${favLength} favorite users!`)
+
+        const br = React.createElement('br')
+
         const lis = users.map(user => {
-            return React.createElement('li',
-                {key: user.username, className: (user.isFavorite ? 'favorite' : '')}, user.name)
+            return React.createElement('li',{
+                key: user.username,
+                className: (user.isFavorite ? 'favorite' : ''),
+                onClick: () => {toggleUser(user)}
+            }, user.name, br, user.username)
         })
-        return React.createElement('ul', null, lis)
+
+
+
+        const ul = React.createElement('ul', null, lis)
+        return React.createElement('div', null, titleLink, ul)
     }
 }
 
